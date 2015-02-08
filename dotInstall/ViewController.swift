4be243d7b34817fee081621c157f5b2f8fd40b17
KoyamaKenichi <KenichiKoyama@MacBook-Air.local>
2015-02-08 23:45:13 +0900

    Initial Commit

---
+++ dotInstall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dotInstall
+//
+//  Created by KoyamaKenichi on 2015/02/08.
+//  Copyright (c) 2015年 Knet21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
