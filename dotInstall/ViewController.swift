a691374c2d8191ef405b576a5a158ee38277fabd
Kenichi Koyama <Kenichi.beru222.Koyama@gmail.com>
2015-02-13 22:16:18 +0900

    Add Lesson#10
    Add Lesson#10 Let's go back to the original screen.

--- dotInstall/ViewController.swift
+++ dotInstall/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func unwindToTop(segue: UIStoryboardSegue) {}
 
     override func viewDidLoad() {
         super.viewDidLoad()
